5c3311c05efa3d00b9f97d78de7a76279d0f13a4
Guillem Casas <gucasassi@gmail.com>
2022-12-13 14:56:55 +0100

    wip(frontend): add register form

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,8 +1,49 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table({ name, email, password });
+  };
+
   return (
     <>
       <h1 className="jumbotron text-center bg-primary">Register</h1>
-      <p>Register page</p>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form className="pt-4" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="form-control mb-4 p-2"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter Name"
+            required
+          />
+          <input
+            type="text"
+            className="form-control mb-4 p-2"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter Email"
+            required
+          />
+          <input
+            type="text"
+            className="form-control mb-4 p-2"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter Password"
+            required
+          />
+          <button className="btn btn-block btn-primary p-2 w-100" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 };
