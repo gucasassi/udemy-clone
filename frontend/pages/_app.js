987f1da782e73475634e03310d6958826d6c3922
Guillem Casas <gucasassi@gmail.com>
2022-12-13 13:26:04 +0100

    styles(frontend): apply styles on home page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,3 +1,8 @@
+import "antd/dist/reset.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd";
+import "../public/css/styles.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <>
-      <h1>Hello World!!</h1>
+      <h1 className="jumbotron text-center bg-primary">Hello World!!</h1>
       <p>from nextjs...</p>
     </>
   );
