a17f0075e48de2b7a372536e50eae4c41b7e768d
Guillem Casas <gucasassi@gmail.com>
2022-12-13 14:25:15 +0100

    wip(frontend): add login and register page

---
+++ frontend/pages/login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary">Login</h1>
+      <p>login page</p>
+    </>
+  );
+};
+
+export default Login;
---
+++ frontend/pages/register.js
@@ -0,0 +1,10 @@
+const Register = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary">Register</h1>
+      <p>Register page</p>
+    </>
+  );
+};
+
+export default Register;
