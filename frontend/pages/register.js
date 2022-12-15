bc0e19b586ac773fadc634bab65293a62db79ba0
Guillem Casas <gucasassi@gmail.com>
2022-12-15 18:28:49 +0100

    feat(front): register user

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState("Guillem");
+  const [email, setEmail] = useState("gucasassi@gmail.com");
+  const [password, setPassword] = useState("1234");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.table({ name, email, password });
+    const { data } = await axios.post(`http://localhost:8000/api/register`, {
+      name,
+      email,
+      password,
+    });
+    console.log("Register response", data);
   };
 
   return (
